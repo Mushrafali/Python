4dfee78e1c3e989c5bf4e9fbacd89ab7331fd68d
Mushrafali Momin <113009766+Mushrafali@users.noreply.github.com>
2024-07-05 21:19:40 +0530

    variables and data types

---
+++ Firstprogram.py
@@ -0,0 +1,25 @@
+print("My name is  Mushrafali.", "I am 23 years old.")
+print("This is my first program in python.")
+print(23)
+print(12+89)
+
+# variables
+
+name = "mushrafali"
+age = 23
+price = 99.9
+
+print(price)
+print(age)
+print("My name is", name)
+
+age2 = age
+
+print(age2)
+
+
+# datatypes
+
+print(type(name))
+print(type(age))
+print(type(price))
