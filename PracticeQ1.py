df130f70c320b9a51a97360ebd7a1c74c6db235a
Mushrafali <mominashraf121@gmail.com>
2024-07-21 19:01:24 +0530

    Q4

--- PracticeQ1.py
+++ PracticeQ1.py
@@ -4,3 +4,27 @@ first = int(input("enter first :"))
 second = int(input("enter second :"))
 
 print("sum =", first + second)
+
+
+#WAP to input side of a square & print its area.
+
+side = float(input("enter square side :"))
+
+print("area =", side * side)  # side * side = side ** 2
+
+
+#WAP to input 2 floating point numbers and print thrie average.
+
+a = float(input("enter first :"))
+b = float(input("enter second :"))
+
+print("average =", (a + b) / 2)
+
+
+# WAP to input 2 integer numbers, a and b.
+# Print True is a is greater than or equal to b. If not print False.
+
+a = int(input("enter first :"))
+b = int(input("enter second :"))
+
+print(a >= b)
