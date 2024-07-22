787ad8360a70f527b201b53d6fdb1ca8c372aaee
Mushrafali <mominashraf121@gmail.com>
2024-07-22 19:01:47 +0530

    Q2

---
+++ PracticeQ2.py
@@ -0,0 +1,9 @@
+#WAP to input user's first name & print its length.
+
+name = input("enter your name :")
+print("lenght of your name is", len(name))
+
+#WAP to find the occurrence of '$' in a String.
+
+str = "Hi, $I am the $ symbol $99.99."
+print(str.count("$"))
